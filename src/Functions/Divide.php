6cbfe66b50a019458412516590f97ad780454224
Anatolii Snegovskii <CarrionGrow@Gmail.com>
2022-02-21 15:26:41 +0300

    Fix

--- src/Functions/Divide.php
+++ src/Functions/Divide.php
@@ -4,8 +4,15 @@ namespace CarrionGrow\FormulaParser\Functions;
 
 class Divide extends FunctionAbstract
 {
+    /**
+     * @throws \Exception
+     */
     public function calculate($left, $right)
     {
+        if ($right == 0) {
+            throw new \Exception('Divide by zero');
+        }
+
         return $left / $right;
     }
 }
